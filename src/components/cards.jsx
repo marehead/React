54ccdbacc27cfd9fd602f4d14b93dc204e0a1d5a
Lopes dos Santos <m.lopes.dos.santos@accenture.com>
2022-11-13 04:24:25 -0300

    subindo card

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,30 +1,35 @@
 import filmes from cards
 
 const filmes = [
-
     {
         id: 1,
+        nome:"Karate Kid",
+        capa: "https://i.pinimg.com/originals/67/a5/39/67a53932833921bb84f734c23d40ecbf.jpg",
+        genero:"Aventura/Drama",
+},
+    {
+        id: 2,
         nome:"WAKANDA",
         capa: "https://th.bing.com/th/id/R.cb29f86b13c0bccdb0d149eb70fe0f13?rik=Kk18IkpjnHrfuA&pid=ImgRaw&r=0",
-        genero:"Ação/Aventura"
+        genero:"Ação/Aventura",
 },
 {
-    id: 2,
+    id: 3,
     nome: "Us",
     capa: "https://th.bing.com/th/id/R.591da9a31f0fc150b1896cf90056a1e2?rik=H%2bNZwXN8y7UTCg&pid=ImgRaw&r=0",
     genero: "terror",
 },
 {
-    id: 3,
+    id: 4,
     nome: "Morro Dos Ventos Uivantes",
     capa: "https://th.bing.com/th/id/R.53eeb490865f6c1ebb59f4b95b3987ce?rik=nFWrW17BDZioqA&riu=http%3a%2f%2f2.bp.blogspot.com%2f-pFOPsDz0bKI%2fUIshuvYCmqI%2fAAAAAAAAE7M%2fsgXeE5lQ-Zg%2fs1600%2fO%2bMorro%2bdos%2bVentos%2bUivantes.jpg&ehk=6lzv5NI2%2b%2f2F9%2fGY4FbQF7gPP3kjykw2Bvsj1QXgYic%3d&risl=&pid=ImgRaw&r=0",
     genero: "Romance",
 },
 {
-        id: 1,
+        id: 5,
         nome:"Mulher Rei",
         capa: "https://th.bing.com/th/id/OIP._Kb5qn5AlzLSAxvAf0UcnAHaJQ?pid=ImgDet&rs=1",
-        genero:"Drama/Ação"
+        genero:"Drama/Ação",
 },
 ]
 
--- src/components/data/profile-data.js
+++ src/components/data/profile-data.js
@@ -1,7 +1,7 @@
 import react from '../assets/react.svg'    
 export const information = {
     nome: "Mare",
-    apelido:"Lopes",
-    foto:'https://s-media-cache-ak0.pinimg.com/736x/2b/23/3e/2b233ee15d204c4ef45a87294b5c83be.jpg',
-    descricao: 'representa a cobra arco-íris, que traz as características do animal como a mobilidade, agilidade e destreza. Ele mora no céu e viaja através do arco-íris para a Terra. Além disso, ele representa a fortuna, abundância, prosperidade e riqueza que são realizações importantes para o seu povo.'
+    apelido:"Nina",
+    foto:'https://avatars.githubusercontent.com/u/96207587?v=4',
+    descrição: 'Me chamo Marina mas pode me chamar de Mare sou queer nobinárie de SP Capital e tenho 21 anos.',
 };
