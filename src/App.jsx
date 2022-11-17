03d433a07b1050ed36ab293eb5a6e146a898c8e0
Lopes dos Santos <m.lopes.dos.santos@accenture.com>
2022-11-16 21:34:35 -0300

    Contador

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
-import './App.css'
-import Formulario from './components/Formulario'
+import './App.css';
+import { useState } from 'react';
 
 function App() {
+    const [contador, setContador] = useState(0);
+
+    const adicionarUm = () => {
+        setContador((contador) => contador + 1);
+    };
+
+    const subtrairUm = () => {
+      setContador((contador) => contador - 1);
+    };
+
+    const traduzirTitulo = () => {
+
+      if (titulo === "Meu primeiro contado") {
+        setTitulo("My first counter!");
+      } else {
+      setTitulo("Meu primeiro contador!");
+    }
+
+    const pt = "";
+    const en = "";
+    titulo === en ? setTitulo(pt) : setTitulo(en); 
+  };
+
+
   return (
-    <div className="app">
-      <Formulario />
+    <div className="App">
+      <h1>Meu Primeiro contador</h1>
+      <button>Traduzir</button>
+      <br />
+      <br />
+      <button onClick={subtrairUm}>-</button>
+        <h1> {contador} </h1>
+        <button onClick={adicionarUm}>+</button>  
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
